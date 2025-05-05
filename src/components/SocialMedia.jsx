0d11456b7d0c94210cf2e53b329c4ffbdba99268
Alwin Mathew <alwinaby2004@gmail.com>
2025-05-05 09:53:43 +0530

    initial

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { BsInstagram, BsGithub, BsLinkedin } from "react-icons/bs";
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <a
+      href="https://www.instagram.com/alwx.n"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div>
+        <BsInstagram />
+      </div>
+    </a>
+    <a
+      href="https://github.com/VirgoTheLord"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div>
+        <BsGithub />
+      </div>
+    </a>
+    <a
+      href="https://www.linkedin.com/in/alwin-aby-mathew-6248b52a2"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div>
+        <BsLinkedin />
+      </div>
+    </a>
+  </div>
+);
+
+export default SocialMedia;
